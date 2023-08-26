46cd53177ca9cc2bcab2bc1bf57d1bb6bc96ea60
qkrckstjq <117289923+qkrckstjq@users.noreply.github.com>
2023-08-26 20:38:37 +0900

    Squashed commit of the following:
    commit 2cfbe3426b3b47cc558c5dd8e44a664bb2998393
    Merge: 941e5e8 b5171e3
    Author: 유명인 <114664128+myeongin0926@users.noreply.github.com>
    Date:   Sat Aug 26 20:35:02 2023 +0900
    
        Merge pull request #3 from codestates-seb/feat/myeongin
    
        [Design] AboutPage
    
    commit b5171e388a72c187ddb1d269c50e5fbdcafba1b5
    Author: myeongin <mungin0926@naver.com>
    Date:   Sat Aug 26 20:32:55 2023 +0900
    
        [Design] AboutPage AboutFirst 배포
    
    commit 8385f0dec63495a9911eeb656a894db575045df4
    Author: myeongin <mungin0926@naver.com>
    Date:   Sat Aug 26 00:54:10 2023 +0900
    
        [Design] Swiper.js 설치 및 About 페이지  디자인
    
    commit 04ffcf4818a837f39efbe8831fdaa76366aab1af
    Author: myeongin <mungin0926@naver.com>
    Date:   Fri Aug 25 23:33:39 2023 +0900
    
        [Feat] 라우팅 함수 생성
    
    commit 941e5e8f106b87da4e9e915d893d9d61af6b26e2
    Merge: ab146c7 1cd2d9a
    Author: Bum <92982989+blue7111@users.noreply.github.com>
    Date:   Fri Aug 25 17:29:32 2023 +0900
    
        Merge pull request #2 from codestates-seb/feat/jongbum
    
        [Init] 초기세팅
    
    commit 1cd2d9a5dda614085c0c04f0f567fb24f6132fa3
    Author: LeeJongBum <qja965@gmail.com>
    Date:   Fri Aug 25 17:24:11 2023 +0900
    
        [Init] 초기세팅

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,5 +1,52 @@
 import React from 'react';
-
+import useNav from '../../hooks/useNav';
+import { StyleBorderButton, StyleBottomButton } from '../common/Buttons';
+import { styled } from 'styled-components';
+import Inner from '../common/Inner';
+const StyleHeader = styled.header`
+  padding: 10px 0;
+  z-index: 10;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  transition: 0.6s;
+  .inner {
+    display: flex;
+  }
+  h1 {
+    font-family: var(--monoton);
+    font-size: 3.5rem;
+    cursor: pointer;
+  }
+  nav {
+    padding-left: 20px;
+    flex: 1;
+    display: flex;
+    gap: 20px;
+  }
+  div {
+    gap: 10px;
+    button {
+      padding: 0 15px;
+    }
+  }
+`;
 export default function Header() {
-  return <div></div>;
+  const { toAbout, toPortfolio, toProject, toSignin, toSignup } = useNav();
+
+  return (
+    <StyleHeader id="header">
+      <Inner>
+        <h1 onClick={toAbout}>SPEC</h1>
+        <nav>
+          <StyleBottomButton onClick={toProject}>프로젝트</StyleBottomButton>
+          <StyleBottomButton onClick={toPortfolio}>포트폴리오</StyleBottomButton>
+        </nav>
+        <div className="row">
+          <StyleBorderButton onClick={toSignin}>로그인</StyleBorderButton>
+          <StyleBorderButton onClick={toSignup}>회원가입</StyleBorderButton>
+        </div>
+      </Inner>
+    </StyleHeader>
+  );
 }
