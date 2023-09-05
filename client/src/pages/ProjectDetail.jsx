be879ffb8bb17e6b4123266c606bf928580f10c8
LeeJongBum <qja965@gmail.com>
2023-09-05 16:41:52 +0900

    [Feat] Profile Valid

--- client/src/pages/ProjectDetail.jsx
+++ client/src/pages/ProjectDetail.jsx
@@ -1,63 +1,59 @@
 import React, { useState } from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components';
 import Page from '../components/common/Page';
 import WriteHeader from '../components/project/WriteHeader';
-import Language from './../components/common/Language';
+import Language from './../components/common/LanguageTag';
 import DetailHead from '../components/project/DetailHead';
 import DetailBody from '../components/project/DetailBody';
 
 const StyleProjectDetailWrapper = styled(Page)`
-  padding-top:6rem;
-  height:500rem;
-`
+  padding-top: 6rem;
+  height: 500rem;
+`;
 
 const StyleProjectDetailContainer = styled.div`
-  width:100%;
-  height:100%;
-  background-color:var(--black-800);
-  border-radius:10px;
-  padding:4rem;
-`
+  width: 100%;
+  height: 100%;
+  background-color: var(--black-800);
+  border-radius: 10px;
+  padding: 4rem;
+`;
 
 const DummyData = {
-  id : 1,
-  title : '프론트엔드 리액트를 사용하여 프로젝트,포트폴리오 공유 프로젝트입니다.',
-  totalPeople : '5',
-  joinPeople : [
+  id: 1,
+  title: '프론트엔드 리액트를 사용하여 프로젝트,포트폴리오 공유 프로젝트입니다.',
+  totalPeople: '5',
+  joinPeople: [
     {
-      userName : '박찬섭',
-      memberId : 1, 
+      userName: '박찬섭',
+      memberId: 1,
     },
     {
-      userName : '박찬섭2',
-      memberId : 2,
+      userName: '박찬섭2',
+      memberId: 2,
     },
     {
-      userName : '박찬섭3',
-      memberId : 3,
-    }
+      userName: '박찬섭3',
+      memberId: 3,
+    },
   ],
-  requestPeople : [
-    {},
-    {},
-    {}
-  ],
-  created_At : "Wed Aug 30 2023 16:07:06 GMT+0900 (한국 표준시)",
-  modified_At : "Wed Aug 30 2023 16:07:06 GMT+0900 (한국 표준시)",
-  closed_At : "Wed Aug 30 2023 16:07:06 GMT+0900 (한국 표준시)",
-  language : 'JAVA',
-  tag : ['테스트태그','의미없는 태그', '의미없는 태그2'],
-  body : '',
-  titleImg : '',
-  imgs : '',
-  description : '',
-  author : {
-    img : '',
-    userName : '박찬섭',
-    id : 1, 
+  requestPeople: [{}, {}, {}],
+  created_At: 'Wed Aug 30 2023 16:07:06 GMT+0900 (한국 표준시)',
+  modified_At: 'Wed Aug 30 2023 16:07:06 GMT+0900 (한국 표준시)',
+  closed_At: 'Wed Aug 30 2023 16:07:06 GMT+0900 (한국 표준시)',
+  language: 'JAVA',
+  tag: ['테스트태그', '의미없는 태그', '의미없는 태그2'],
+  body: '',
+  titleImg: '',
+  imgs: '',
+  description: '',
+  author: {
+    img: '',
+    userName: '박찬섭',
+    id: 1,
   },
-  likes : ["1", "2", "3", "4", "5"],
-}
+  likes: ['1', '2', '3', '4', '5'],
+};
 
 // {
 //   "titleImg": "",
@@ -79,9 +75,9 @@ export default function ProjectDetail() {
   const [detailData, setDetailData] = useState(DummyData);
   return (
     <StyleProjectDetailWrapper>
-      <StyleProjectDetailContainer className='col'>
-        <DetailHead detailData={detailData}/>
-        <DetailBody/>
+      <StyleProjectDetailContainer className="col">
+        <DetailHead detailData={detailData} />
+        <DetailBody />
       </StyleProjectDetailContainer>
     </StyleProjectDetailWrapper>
   );
