7dd44bcc7785923c23ed86d39a821f569d7d676f
LeeJongBum <qja965@gmail.com>
2023-09-11 22:19:16 +0900

    [Fix] Profile Design

---
+++ client/src/hooks/useMideaQuery.jsx
@@ -0,0 +1,23 @@
+// import React from 'react';
+// import { useMediaQuery } from 'react-responsive';
+
+// export const PC = ({ children }) => {
+//   const isPC = useMediaQuery({
+//     query: '(max-width : 1250px)',
+//   });
+//   return <>{isPC && children}</>;
+// };
+
+// export const Tablet = ({ children }) => {
+//   const isTablet = useMediaQuery({
+//     query: '(max-width : 768px)',
+//   });
+//   return <>{isTablet && children}</>;
+// };
+
+// export const Mobile = ({ children }) => {
+//   const isMobile = useMediaQuery({
+//     query: '(max-width : 520px)',
+//   });
+//   return <>{isMobile && children}</>;
+// };
