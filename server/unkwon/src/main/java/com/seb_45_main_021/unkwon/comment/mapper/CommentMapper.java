f65aebae1c2a5788c576ad5e07d45c3a0cc8e6e4
hwiyong <gksdyd0923@gmail.com>
2023-09-12 18:45:30 +0900

    [Fix] response 수정

--- server/unkwon/src/main/java/com/seb_45_main_021/unkwon/comment/mapper/CommentMapper.java
+++ server/unkwon/src/main/java/com/seb_45_main_021/unkwon/comment/mapper/CommentMapper.java
@@ -23,7 +23,7 @@ public interface CommentMapper {
         Comment comment = new Comment();
         comment.setMember(member);
         comment.setPortFolio(portFolio);
-        comment.setContent(commentPostDto.getContent());
+        comment.setBody(commentPostDto.getBody());
 
         return comment;
     }
@@ -34,7 +34,7 @@ public interface CommentMapper {
 
         CommentDto.ResponseDto commentResponseDto = new CommentDto.ResponseDto(
                 comment.getCommentId(),
-                comment.getContent(),
+                comment.getBody(),
                 comment.getPortFolio().getPortfolioId(),
                 comment.getMember().getMemberId(),
                 comment.getMember().getEmail(),
